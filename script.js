b7c991850eacfa00cf58e5322d8433f8e94c1dd8
Polianoo <polianorodrigues944@outlook.com>
2024-06-26 23:11:27 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function trocaCor(cor) {
+
+    const circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+
+}
+
+trocaCor()
+
+
+
+
+
+function trocaImagem(imagem) {
+
+    const imgIphone = document.querySelector(".iphone") 
+    imgIphone.src = imagem
+
+}
+trocarImagem()
